599a6d1ce6be27e1c5d6799bbbc0726976161ca8
Arun Gnawali <078bel021.arun@pcampus.edu.np>
2025-02-03 16:02:18 +0545

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ def fetch_poster(movie_id):
     response=requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(movie_id))
     data=response.json()
     return "https://image.tmdb.org/t/p/w500/"+data['poster_path']
+import os
+import pickle
+
+file_path = os.path.join(os.getcwd(), 'similarity.pkl')
+similarity = pickle.load(open(file_path, 'rb'))
 
 
 movies_dict=pickle.load(open('movie_dict.pkl','rb'))
