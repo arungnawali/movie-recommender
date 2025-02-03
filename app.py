b31619801ac395c5a4f2a151731c39841064e26c
Arun Gnawali <078bel021.arun@pcampus.edu.np>
2025-02-04 00:56:25 +0545

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,12 @@ def recommend(movie):
         st.error("Error: similarity_data is not loaded correctly.")
         return [], []
     
-    movie_index = movies[movies['title'] == movie].index[0]
+    try:
+        movie_index = movies[movies['title'] == movie].index[0]
+    except IndexError:
+        st.error("Error: Movie not found.")
+        return [], []
+
     distances = similarity_data[movie_index]
     movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
     
@@ -65,24 +70,25 @@ selected_movie = st.selectbox("Select the movie", movies['title'].values)
 # Recommend movies
 if st.button("Recommend"):
     names, posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
+    if names and posters:
+        col1, col2, col3, col4, col5 = st.columns(5)
 
-    with col1:
-        st.text(names[0])
-        st.image(posters[0])
+        with col1:
+            st.text(names[0])
+            st.image(posters[0])
 
-    with col2:
-        st.text(names[1])
-        st.image(posters[1])
+        with col2:
+            st.text(names[1])
+            st.image(posters[1])
 
-    with col3:
-        st.text(names[2])
-        st.image(posters[2])
+        with col3:
+            st.text(names[2])
+            st.image(posters[2])
 
-    with col4:
-        st.text(names[3])
-        st.image(posters[3])
+        with col4:
+            st.text(names[3])
+            st.image(posters[3])
 
-    with col5:
-        st.text(names[4])
-        st.image(posters[4])
+        with col5:
+            st.text(names[4])
+            st.image(posters[4])
