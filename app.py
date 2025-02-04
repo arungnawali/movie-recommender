7bfbdac3d7ecf2073dc85912c5c808c27a3654fd
Arun Gnawali <078bel021.arun@pcampus.edu.np>
2025-02-04 11:04:18 +0545

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,7 @@
 import pickle
 import streamlit as st
 import requests
-from urllib.request import urlopen
-similarityURL = "https://drive.google.com/uc?export=download&id=1LxuSIP7Z35d1pEu5wAjuei4GARB7YKJh"
-similarityRES = urlopen(similarityURL)
-similarity = pickle.load(open(similarityRES,'rb'))
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
@@ -22,7 +12,7 @@ def recommend(movie):
     recommended_movie_posters = []
     for i in distances[1:6]:
         # fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
+        movie_id = movies.iloc[i[0]].id
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
 
@@ -30,8 +20,7 @@ def recommend(movie):
 
 
 st.header('Movie Recommender System')
-movies = pickle.load(open('model/movie_list.pkl','rb'))
-similarity = pickle.load(open('model/similarity.pkl','rb'))
+movies = pickle.load(open('movie_list.pkl','rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
