19b15b1694091a885d12c29e4077fef4fa035ed7
Arun Gnawali <078bel021.arun@pcampus.edu.np>
2025-02-04 11:07:35 +0545

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,139 @@
 import pickle
 import streamlit as st
 import requests
+import numpy as np
+import pandas as pd
+movies=pd.read_csv('/content/tmdb_5000_movies.csv',on_bad_lines='skip', engine='python')
+credits=pd.read_csv('/content/tmdb_5000_credits.csv',on_bad_lines='skip', engine='python')
+movies=pd.merge(movies,credits,on='title')
+movies.head(1)
+
+print("numpy")
+
+import pandas as pd
+import numpy as np
+import sklearn
+import ast
+import nltk
+
+print("numpy",np.__version__)
+print("pandas",pd.__version__)
+print("sklearn",sklearn.__version__)
+print("nltk",nltk.__version__)
+print("ast",ast.__version__)
+
+#genres
+#id
+#keywords
+#original_title
+#overview
+#cast
+#crew
+
+#extracting required columns
+movies=movies[['genres','id','keywords','title','overview','cast','crew']]
+movies.isnull().sum()
+movies.dropna(inplace=True)
+
+import ast
+def convert(obj):
+  L=[]
+  for i in ast.literal_eval(obj):
+    L.append(i['name'])
+  return L
+
+movies['genres']=movies['genres'].apply(convert)
+
+movies['keywords']=movies['keywords'].apply(convert)
+
+import ast
+def convert3(obj):
+  L=[]
+  counter=0
+  for i in ast.literal_eval(obj):
+    if(counter!=3):
+      L.append(i['name'])
+      counter+=1
+    else:
+      break
+  return L
+
+movies['cast']=movies['cast'].apply(convert3)
+
+movies.head()
+
+import ast
+def convertdir(obj):
+  L=[]
+  for i in ast.literal_eval(obj):
+    if(i['job']=="Director"):
+      L.append(i['name'])
+
+    else:
+      continue
+  return L
+
+movies['crew']=movies['crew'].apply(convertdir)
+
+movies.head()
+
+movies['overview']=movies['overview'].apply(lambda x:x.split())
+
+movies.head()
+
+movies['genres']=movies['genres'].apply(lambda x:[i.replace(" ","") for i in x])
+
+movies['keywords']=movies['keywords'].apply(lambda x:[i.replace(" ","") for i in x])
+movies['cast']=movies['cast'].apply(lambda x:[i.replace(" ","") for i in x])
+movies['crew']=movies['crew'].apply(lambda x:[i.replace(" ","") for i in x])
+
+movies['tags']=movies['overview']+movies['genres']+movies['keywords']+movies['cast']+movies['crew']
+
+new_df=movies[['id','title','tags']]
+
+new_df['tags']=new_df['tags'].apply(lambda x:" ".join(x))
+
+new_df['tags']=new_df['tags'].apply(lambda x:x.lower())
+
+import nltk
+from nltk.stem.porter import PorterStemmer
+ps=PorterStemmer()
+
+def stem(text):
+  y=[]
+  for i in text.split():
+    y.append(ps.stem(i))
+  return " ".join(y)
+
+new_df['tags']=new_df['tags'].apply(stem)
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv=CountVectorizer(stop_words='english',max_features=5000)
+vectors=cv.fit_transform(new_df["tags"]).toarray()
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+import numpy as np
+
+np.set_printoptions(threshold=10)  # Display only 5 elements
+
+similarity=cosine_similarity(vectors)
+sorted(list(enumerate(similarity[0])),reverse=True,key=lambda x:x[1])
+
+def recommend(movie):
+  movie_index=new_df[new_df['title']==movie].index[0]
+  distances=similarity[movie_index]
+  movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+  for i in movies_list:
+
+    print(new_df.iloc[i[0]].title)
+
+recommend("Avatar")
+
+import pickle
+
+pickle.dump(new_df.to_dict(),open('movie_dict.pkl','wb'))
+
 
 
 def recommend(movie):
