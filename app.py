057ca4fc44a62e4446e45cdafe9a06748d1ed9a2
Arun Gnawali <078bel021.arun@pcampus.edu.np>
2025-02-04 11:21:07 +0545

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 import requests
 import numpy as np
 import pandas as pd
-movies=pd.read_csv('/content/tmdb_5000_movies.csv',on_bad_lines='skip', engine='python')
+movies=pd.read_csv('tmdb_5000_movies.csv',on_bad_lines='skip', engine='python')
 credits=pd.read_csv('/content/tmdb_5000_credits.csv',on_bad_lines='skip', engine='python')
 movies=pd.merge(movies,credits,on='title')
 movies.head(1)
