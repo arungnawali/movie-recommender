2a774d3bc1fae7ebae53f28f592be2562f2143b9
Arun Gnawali <078bel021.arun@pcampus.edu.np>
2025-02-04 12:12:11 +0545

    Update app.py

--- app.py
+++ app.py
@@ -104,16 +104,13 @@ np.set_printoptions(threshold=10)  # Display only 5 elements
 similarity=cosine_similarity(vectors)
 sorted(list(enumerate(similarity[0])),reverse=True,key=lambda x:x[1])
 
-def recommend(movie):
-  movie_index=new_df[new_df['title']==movie].index[0]
-  distances=similarity[movie_index]
-  movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
-  for i in movies_list:
-
-    print(new_df.iloc[i[0]].title)
-
-recommend("Avatar")
-
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
 
 
 def recommend(movie):
